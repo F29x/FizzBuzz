f43910016a4c18121bcf0637f37d0c1bc2c34ebe
Franci <f.dhima29@gmail.com>
2024-04-14 20:55:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var output = [];
+
+
+function fizzBuzz() {
+
+    for(var count = 1; count<=100; count++){
+
+    
+    if(count % 3 === 0 && count % 5 === 0){
+        output.push("FizzBuzz");
+    }
+    
+    else if(count % 3===0){
+        output.push("Fizz");
+    }
+    
+    
+    else if(count % 5 === 0){
+        output.push("Buzz");
+    }
+    else{
+        output.push(count);
+    }
+    
+    
+    }
+    document.getElementById('text').value= output;
+    
+}
+
+var buton = document.querySelector("button");
+
+buton.addEventListener("click",fizzBuzz);
+
+
+
